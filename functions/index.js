6f41a1e5738187a38648281e6c8ee7a1176993db
Muhammad Azhar Iqbal <muhammad1azhar2@gmail.com>
2022-11-01 22:24:15 +0500

    solved firebase cloud func errors and deployed functions.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable indent */
-/* eslint-disable linebreak-style */
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const serviceAccount = require("./mimi-plan-test.json");
@@ -8,86 +6,68 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  // eslint-disable-next-line object-curly-spacing
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
-exports.addData = functions.https.onRequest((request, response) => {
-  // eslint-disable-next-line max-len
-  const result = admin.firestore().collection("testadmin").doc("testusers").set({ value: "some value" });
-  response.status(200).send(result);
-});
-
 exports.deleteUser = functions.https.onCall((data, context) => {
   return new Promise((resolve, reject) => {
-    admin.auth()
-      .deleteUser(data.id)
-      .then(() => {
-        resolve("Staff is deleted successfully!");
-      })
-      .catch((error) => {
-        reject(new functions.https.HttpsError(error.code, error.message));
-        console.log(error);
-      });
+    admin
+        .auth()
+        .deleteUser(data.id)
+        .then(() => {
+          resolve("Staff is deleted successfully!");
+        })
+        .catch((error) => {
+          reject(new functions.https.HttpsError(error.code, error.message));
+          console.log(error);
+        });
   });
 });
 
 exports.disableRoutePlan = functions.https.onCall((data, context) => {
+
   return new Promise(async (resolve, reject) => {
-
     try {
+      await admin
+        .firestore()
+        .collection("Institution")
+        .doc(data.doc)
+        .collection("routePlan")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach(async (doc) => {
+            admin
+              .firestore()
+              .collection("Institution")
+              .doc(data.doc)
+              .collection("routePlan")
+              .doc(doc.id)
+              .onSnapshot((snapshot) => {
+                if (
+                  moment(snapshot.data().endingDate?.toDate()).format(
+                    "DD-MM-YYYY"
+                  ) <= moment(new Date()).format("DD-MM-YYYY")
+                ) {
+                  admin
 
-
-
-
-
-      await admin.firestore().collection("Institution").doc(data.doc).collection("routePlan").get().then(querySnapshot => {
-        querySnapshot.forEach(async (doc) => {
-
-          admin.firestore().collection("Institution")
-            .doc(data.doc)
-            .collection("routePlan")
-            .doc(doc.id)
-            .onSnapshot((snapshot) => {
-              if (moment(snapshot.data().endingDate?.toDate()).format('DD-MM-YYYY') <= moment(new Date()).format('DD-MM-YYYY')) {
-
-
-                admin.firestore().collection('Institution').doc(data.doc).collection('routePlan').doc(snapshot.data().id).update({
-                  status: false,
-                }).then((res => {
-                  resolve("Route is disabled")
-                }))
-
-
-              }
-
-            })
-
+                    .firestore()
+                    .collection("Institution")
+                    .doc(data.doc)
+                    .collection("routePlan")
+                    .doc(snapshot.data().id)
+                    .update({
+                      status: false,
+                    })
+                    .then((res) => {
+                      resolve("Route is disabled");
+                    });
+                }
+              });
+          });
         });
-
-
-      })
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-    catch {
+    } catch {
+      // eslint-disable-next-line no-unused-expressions
       (e) => {
-        reject(e)
-      }
-
+        reject(e);
+      };
     }
-
-
   });
 });
+
